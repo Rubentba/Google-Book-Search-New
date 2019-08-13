edace6d312b770b5e3af51c946f6e5b4c672d65b
Rubentba <rubenatorresbeltran@gmail.com>
2019-08-13 18:25:35 -0400

    Redid API to include querying with google api key.

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,9 +1,15 @@
 import axios from 'axios';
+import APIKEY from '../config/keys';
+const URL = 'https://www.googleapis.com/books/v1/volumes?q='
 
 export default {
     // Gets all books
-    getBooks: function () {
-        return axios.get('/api/books');
+    searchBooks: function (query) {
+        return axios.get(URL + query + APIKEY);
+    },
+    // Gets the book with the given id
+    getBook: function () {
+        return axios.get('/api/books/');
     },
     // Gets the book with the given id
     getBook: function (id) {
